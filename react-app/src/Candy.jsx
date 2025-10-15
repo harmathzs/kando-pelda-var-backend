45433875205dbfe3b048063a5dbfc22d36de4325
Zsolt Harmath <harmathzs@gmail.com>
2025-10-15 20:31:19 +0200

    Style Candy card

--- react-app/src/Candy.jsx
+++ react-app/src/Candy.jsx
@@ -1,10 +1,10 @@
 /* Candy.jsx */
 const Candy = ({name, mass, origin}) => (
-    <div>
-        <p><strong>Name:</strong> {name}</p>
-        <p><strong>Mass:</strong> {mass}</p>
-        <p><strong>Origin:</strong> {origin}</p>
-        <hr />
-    </div>
+  <div className="candy-card">
+    <p><strong>Name:</strong> {name}</p>
+    <p><strong>Mass:</strong> {mass}</p>
+    <p><strong>Origin:</strong> {origin}</p>
+  </div>
 )
+
 export default Candy
