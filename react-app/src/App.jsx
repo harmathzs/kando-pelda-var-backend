643b5b61790244b30d0dc33641460535c3f853a0
Zsolt Harmath <harmathzs@gmail.com>
2025-10-15 18:22:54 +0200

    Show candies

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import Candy from './Candy'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default class App extends React.Component {
+  state = {
+    candies: []
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  async fetchCandies() {
+    const candiesJson = await fetch('http://localhost:3000/candies')
+    const candies = await candiesJson.json()
+    console.log(candies)
+    this.setState({candies})
+  }
+
+  componentDidMount() {
+    this.fetchCandies()
+  }
+
+  handleDeleteAll = async (e) => {
+    await fetch('http://localhost:3000/candies', {method: 'DELETE'})
+    this.fetchCandies()
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Candies</h1>
+        <div className="card">
+          {this.state.candies.map(({name, mass, origin})=><Candy key={name} name={name} mass={mass} origin={origin} />)}
+        </div>
+        <div className='card'>
+          <button onClick={this.handleDeleteAll}>Delete All candies</button>
+        </div>
+      </>
+    )
+  }
 }
 
-export default App
---
+++ react-app/src/Candy.jsx
@@ -0,0 +1,9 @@
+const Candy = ({name, mass, origin}) => (
+    <div>
+        <p><strong>Name:</strong> {name}</p>
+        <p><strong>Mass:</strong> {mass}</p>
+        <p><strong>Origin:</strong> {origin}</p>
+        <hr />
+    </div>
+)
+export default Candy
